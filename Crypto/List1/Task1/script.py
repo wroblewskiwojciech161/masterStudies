bbd4d399bc9253ecbb9e8cb1eb0f6e06702b91e3
Wojciech Wroblewski <wroblewskiwojciech161@gmail.com>
2022-03-23 00:53:41 +0100

    reinitialized

--- Crypto/List1/Task1/script.py
+++ Crypto/List1/Task1/script.py
@@ -1,67 +1,40 @@
 #!/usr/bin/env python3
-# Script by Aleksander Lasecki
 
 import argparse
 from Crypto.PublicKey import RSA
 
 
-def egcd(a, b):
-    x, y = 0, 1
-    u, v = 1, 0
+def create_private_key(format='utf-8'):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('P', type=int)
+    parser.add_argument('Q', type=int)
+    parser.add_argument('E', type=int)
+    args = parser.parse_args()
+    '''
+    p - first modulus factor
+    q - second modulus factor
+    e - public exponent
+    '''
+    p = args.P
+    q = args.Q
+    e = args.E
 
-    steps = 0
-
-    # print("    \\[\n\ta = {},\\ b = {},\\ x = {},\\ y = {},\\ u = {},\\ v = {}\n    \\]".format(a, b, x, y, u, v))
-
-    while a != 0:
-        steps += 1
-
-        q, r = b // a, b % a
-        m, n = x - u * q, y - v * q
-
-        b, a = a, r
-        x, y = u, v
-        u, v = m, n
-
-        # print("    \\[\n\ta = {},\\ b = {},\\ x = {},\\ y = {},\\ u = {},\\ v = {}\n    \\]".format(a, b, x, y, u, v))
-
-    gcd = b
-    return gcd, x, y, steps
-
-# key = RSA.importKey(open('files/publickey.pem').read())
-# p = 1524938362073628791222322453937223798227099080053904149
-# q = 1385409854850246784644682622624349784560468558795524903
-
-# private_key = RSA.construct((
-#     key.n,
-#     key.e,
-#     pow(key.e, -1, (p - 1) * (q - 1)),
-#     p,
-#     q
-# ))
-
-# key_text = private_key.exportKey()
-# print(key_text)
+    '''
+    construct RSA object with RSA lib
+    retrive private key && decode as utf-8
+    '''
+    print("dupa \n")
+    print(RSA.construct((p * q, e, pow(e, -1, (p - 1) * (q - 1)), p, q)
+                        ).publickey().export_key("OpenSSH"))
+    return RSA.construct((p * q, e, pow(e, -1, (p - 1) * (q - 1)), p, q)).exportKey().decode(format)
 
 
-parser = argparse.ArgumentParser(
-    description="Generate private key files from numeric data (p, q, e)")
-parser.add_argument('p', type=int, help="Larger prime factor")
-parser.add_argument('q', type=int, help="Smaller prime factor")
-parser.add_argument('e', type=int, help="Public exponent")
-args = parser.parse_args()
+def main():
 
-p = args.p
-q = args.q
-e = args.e
+    private = create_private_key()
+    print("Generated key: \n")
+    print(private)
 
-private_key = RSA.construct((
-    p * q,
-    e,
-    pow(e, -1, (p - 1) * (q - 1)),
-    p,
-    q
-))
 
-key_text = private_key.exportKey()
-print(key_text.decode('utf-8'))
+if __name__ == "__main__":
+    main()
